9831742c0b8f7a5e648439b27516dd48f18a8988
Felipe Basini <88778207+fbasini@users.noreply.github.com>
2025-03-12 00:01:53 -0300

    Agrego redis

--- BibliotecaAPI/Program.cs
+++ BibliotecaAPI/Program.cs
@@ -16,15 +16,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // servicios
 
-builder.Services.AddOutputCache(opciones =>
-{
-    opciones.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(60);
-});
-
-//builder.Services.AddStackExchangeRedisOutputCache(opciones =>
+//builder.Services.AddOutputCache(opciones =>
 //{
-//    opciones.Configuration = builder.Configuration.GetConnectionString("redis");
+//    opciones.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(60);
 //});
+
+builder.Services.AddStackExchangeRedisOutputCache(opciones =>
+{
+    opciones.Configuration = builder.Configuration.GetConnectionString("redis");
+});
 
 builder.Services.AddDataProtection();
 
